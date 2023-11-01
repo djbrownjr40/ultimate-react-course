f7250fdac860f116376fe06423225e5c01f040a3
Dennis Brown djbrownjr.40@gmail.com <djbrownjr40@gmail.com>
2023-11-02 00:07:09 +0900

    cleanup

--- react-profile-card/src/index.js
+++ react-profile-card/src/index.js
@@ -1,7 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "intermediate",
+    color: "#2662EA"
+  },
+  {
+    skill: "JavaScript",
+    level: "beginner",
+    color: "#EFD81D"
+  },
+  {
+    skill: "Web Design",
+    level: "beginner",
+    color: "#C3DCAF"
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33"
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB"
+  },
+  {
+    skill: "Ruby on Rails",
+    level: "intermediate",
+    color: "orange"
+  }
+];
 
 function App() {
   return (
@@ -9,7 +41,9 @@ function App() {
       <Avatar />
       <div className="data">
         <Intro />
-
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
         <SkillList />
       </div>
     </div>
@@ -17,47 +51,48 @@ function App() {
 }
 
 function Avatar() {
-  return <img className="avatar" src="avatar.jpg" alt="Dennis" />;
+  return <img className="avatar" src="avatar.jpg" alt="Dennis John Brown Jr." />;
 }
 
-function Intro (){
+function Intro() {
   return (
     <div>
       <h1>Dennis John Brown Jr.</h1>
-      <p>I'm Dennis, a Tokyo-based Full Stack Web Developer passionate about creating dynamic and user-centric web applications. As a graduate of the Le Wagon web developer bootcamp, I've recently honed my skills in JavaScript and Ruby on Rails and am eager to take on new challenges in web development.</p>
+      <p>
+      I'm Dennis, a Tokyo-based Full Stack Web Developer passionate about creating dynamic and user-centric web applications. As a graduate of the Le Wagon web developer bootcamp, I've recently honed my skills in JavaScript and Ruby on Rails and am eager to take on new challenges in web development.
+      </p>
     </div>
-  )
+  );
 }
 
-function SkillList(){
+function SkillList() {
   return (
     <div className="skill-list">
-      <Skill skill="React" emoji="⚛" color="orangered"/>
-      <Skill skill="JavaScript" emoji="🔋" color="yellow" />
-      <Skill skill="HTML" emoji="📕" color="skyblue" />
-      <Skill skill="CSS" emoji="❤️‍🔥" color="pink" />
-      <Skill skill="Ruby on Rails" emoji="💎" color="red" />
-
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
     </div>
-  )
+  );
 }
 
-function Skill(props){
-  return <div className="skill" style={{backgroundColor: props.color}}>
-    <span>{props.skill}</span>
-    <span>{props.emoji}</span>
-
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
